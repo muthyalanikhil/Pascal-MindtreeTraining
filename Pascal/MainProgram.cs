93da4ecf0439abfd8a1de8af90b87632c9b581da
nikhil muthyala <s528754@nwmissouri.edu>
2018-02-16 21:06:39 -0600

    uploading to github

---
+++ Pascal/MainProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pascal
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            PascalTriangle obj1 = new PascalTriangle();
+            obj1.PascalLoop();
+        }
+    }
+}
---
+++ Pascal/PascalTriangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pascal
+{
+    class PascalTriangle
+    {
+        public void PascalLoop()
+        {
+            try
+            {
+                int n;
+                n = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < n; i++)
+                {
+                    for (int k = (n - i - 1); k >= 0; k--)
+                    {
+                        Console.Write(" ");
+                    }
+                    int num = 1;
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write(num);
+                        Console.Write(" ");
+                        num = num * (i - j) / (j + 1);
+                    }
+
+                    Console.WriteLine(" ");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); ;
+            }
+        }
+    }
+}
